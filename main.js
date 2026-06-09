070b35f3f1a6d77b8ae97cc605f0f80b0f5c1f20
ketting1 <rasfreeman@hotmail.com>
2026-06-10 02:55:21 +1000

    Implement animateValue function for dynamic counting
    Add animateValue function to animate numerical values on the page.

--- main.js
+++ main.js
@@ -18,3 +18,36 @@ function updateClock() {
 }
 
 setInterval(updateClock,1000);
+function animateValue(id,start,end,duration){
+
+let current = start;
+
+const increment = end / (duration / 20);
+
+const obj = document.getElementById(id);
+
+const timer = setInterval(function(){
+
+current += increment;
+
+if(current >= end){
+
+current = end;
+
+clearInterval(timer);
+
+}
+
+obj.innerHTML = Math.floor(current);
+
+},20);
+
+}
+
+animateValue("aiCount",0,100,2000);
+
+animateValue("courseCount",0,50,2000);
+
+animateValue("memberCount",0,10000,2500);
+
+animateValue("countryCount",0,190,2000);
